fc2d8aa9db692c9db09ea7194891e8eb7485c4c0
Alexis Beingessner <a.beingessner@gmail.com>
2015-07-25 11:55:21 -0700

    done first list

--- lists/src/first.rs
+++ lists/src/first.rs
@@ -40,6 +40,15 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
